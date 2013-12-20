2d7bd97bfeec81a273e89733632f163135594ca3
Ryutaro YOSHIBA <ryuzee@gmail.com>
2013-12-21 07:39:00 +0900

    change dependency for yum

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,7 @@ license          'MIT'
 description      'Installs/Configures ruby-env'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
-depends          'yum'
+depends          'yum', '>= 3.0'
 depends          'build-essential'
 depends          'git'
 depends          'nginx'
